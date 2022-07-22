01838d061733bd67d0c6f949a5b3120d47d6cb67
ElkinAndrey <andreyridbir@gmail.com>
2022-07-22 18:27:29 +0500

    Добавьте файлы проекта.

---
+++ TextUpsideDown/TextUpsideDown/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TextUpsideDown
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnButtonPressed(object sender, EventArgs args)
+        {
+            var text = input.Text;
+            output.Text = Translate.InvertedText(text);
+        }
+    }
+}
