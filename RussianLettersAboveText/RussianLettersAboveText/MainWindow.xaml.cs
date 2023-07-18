000588ff2532c7718430a4f4256326d182f9184e
ElkinAndrey <andreyridbir@gmail.com>
2023-07-18 23:58:41 +0500

    Проект. Пишет текст маленькими буквами над основным текстом

---
+++ RussianLettersAboveText/RussianLettersAboveText/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RussianLettersAboveText
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int fontSize = 14;
+
+        private int FontSize 
+        { 
+            get 
+            { 
+                return fontSize; 
+            } 
+            set
+            {
+                fontSize = value;
+                result.FontSize = fontSize;
+                downText.FontSize = fontSize;
+                upText.FontSize = fontSize;
+            } 
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            FontSize = 30;
+            result.Text = "a";
+            for (int i = 0; i < 300; i++)
+                result.Text +=((char)11766).ToString();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            result.Text = WorkWithText.RussianLettersAboveText(downText.Text, upText.Text);
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+            result.Text = WorkWithText.RussianLettersAboveText(downText.Text, upText.Text);
+        }
+    }
+}
---
+++ RussianLettersAboveText/RussianLettersAboveText/WorkWithText.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RussianLettersAboveText
+{
+    public static class WorkWithText
+    {
+        public static string RussianLettersAboveText(string text, string upText)
+        {
+            Dictionary<char, int> upLettets = new Dictionary<char, int>()
+            {
+                { 'а', 11766 },
+                { 'б', 11744 },
+                { 'в', 11745 },
+                { 'г', 11746 },
+                { 'д', 11747 },
+                { 'е', 11767 },
+                { 'ё', 11767 },
+                { 'ж', 11748 },
+                { 'з', 11749 },
+                { 'и', 42613 },
+                { 'й', 42613 },
+                { 'к', 11750 },
+                { 'л', 11751 },
+                { 'м', 11752 },
+                { 'н', 11753 },
+                { 'о', 11754 },
+                { 'п', 11755 },
+                { 'р', 11756 },
+                { 'с', 11757 },
+                { 'т', 11758 },
+                { 'у', 42615 },
+                { 'х', 11759 },
+                { 'ц', 11760 },
+                { 'ч', 11761 },
+                { 'ш', 11762 },
+                { 'щ', 11763 },
+                { 'ъ', 42616 },
+                { 'ы', 42617 },
+                { 'ь', 42618 },
+            };
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in text.Zip(upText, (textLetter, upTextLetter) => (textLetter, upTextLetter)))
+            {
+                sb.Append(item.textLetter);
+                try
+                {
+                    sb.Append(((char)upLettets[Char.ToLower(item.upTextLetter)]).ToString());
+                }
+                catch { }
+            }
+
+            if (text.Length > upText.Length)
+                sb.Append(text.Substring(upText.Length, text.Length - upText.Length));
+
+            return sb.ToString();
+        }
+    }
+}
